abaca8aa7665ff77521f6e3c99b39429e5d40d54
Simon Schmidt <pomminger@gmail.com>
2020-10-24 13:35:26 +0200

    +

--- can/interfaces/nixnet/_utils.py
+++ can/interfaces/nixnet/_utils.py
@@ -58,35 +58,6 @@ class CanComm(_CanComm):
     pass
 
 
-
-def flatten_items(_list):
-    # type: (typing.Union[typing.Text, typing.List[typing.Text]]) -> typing.Text
-    """Flatten an item list to a string
-
-    >>> str(flatten_items('Item'))
-    'Item'
-    >>> str(flatten_items(['A', 'B']))
-    'A,B'
-    >>> str(flatten_items(None))
-    ''
-    """
-    if isinstance(_list, six.string_types):
-        # For FRAME_IN_QUEUED / FRAME_OUT_QUEUED
-        # Convenience for everything else
-        if "," in _list:
-            _errors.raise_xnet_error(_cconsts.NX_ERR_INVALID_PROPERTY_VALUE)
-        flattened = _list
-    elif isinstance(_list, collections.Iterable):
-        flattened = ",".join(_list)
-    elif _list is None:
-        # For FRAME_IN_STREAM / FRAME_OUT_STREAM
-        flattened = ""
-    else:
-        _errors.raise_xnet_error(_cconsts.NX_ERR_INVALID_PROPERTY_VALUE)
-
-    return flattened
-
-
 def parse_can_comm_bitfield(bitfield):
     # type:  (int) -> CanComm
     """Parse a CAN Comm bitfield."""
